1e3877e225d8a710e5a26778d97634098b5dcbd5
wendellrodrigues <wendell.rodrigues@sjsu.edu>
2021-02-23 20:29:21 -0800

    Add Find Size
    Create route to find size which sends a GET request to arduino (lights up LED on particular shelf)

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,24 +1,21 @@
-const admin           = require('firebase-admin');
-const serviceAccount  = require('../FirebaseAccountKeys.json')
-const users           = require('../users/users')
+const admin = require("firebase-admin");
+const serviceAccount = require("../FirebaseAccountKeys.json");
+const users = require("../users/users");
+const axios = require("axios");
+var http = require("http");
 
+const db = admin.firestore();
 
-const db = admin.firestore()
-
-module.exports = { 
-
+module.exports = {
   /**
    * Sends a test message
    * @param   req   The request
    * @param   res   The response
-   * 
+   *
    */
-  sendTestMsg: async(req, res) => {
-    res.json({name: "Test message"})
+  sendTestMsg: async (req, res) => {
+    res.json({ name: "Test message" });
   },
-
-
-
 
   /**
 
@@ -43,21 +40,17 @@ module.exports = {
     }
     
   */
-  getUser: async(req, res) => {
+  getUser: async (req, res) => {
+    const userId = req.body.userId;
 
-    const userId = req.body.userId
-
-    if(!userId) {
-      res.status(400).send()
-      return
+    if (!userId) {
+      res.status(400).send();
+      return;
     }
 
-    const user = await users.getUserWithId(userId)
-    res.json(user)
-
+    const user = await users.getUserWithId(userId);
+    res.json(user);
   },
-
-
 
   /**
 
@@ -72,24 +65,20 @@ module.exports = {
 
     
   */
-  getShelf: async(req, res) => {
+  getShelf: async (req, res) => {
+    const beaconId = req.body.beaconId;
+    const major = req.body.beaconMajor;
+    const minor = req.body.beaconMinor;
 
-    const beaconId = req.body.beaconId
-    const major = req.body.beaconMajor
-    const minor = req.body.beaconMinor
-
-    if( !beaconId ||
-    !major ||
-    !minor) {
-      res.status(400).send()
-      return
+    if (!beaconId || !major || !minor) {
+      res.status(400).send();
+      return;
     }
 
-    const shelf = await users.getShelf(beaconId, major, minor)
+    const shelf = await users.getShelf(beaconId, major, minor);
 
-    res.json(shelf)
+    res.json(shelf);
   },
-
 
   /**
 
@@ -102,18 +91,16 @@ module.exports = {
 
     
   */
-  getShelfWithId: async(req, res) => {
-    const uid =  req.body.uid
-    if(!uid) {
-      res.status(400).send()
-      return
+  getShelfWithId: async (req, res) => {
+    const uid = req.body.uid;
+    if (!uid) {
+      res.status(400).send();
+      return;
     }
-    const shelf = await users.getShelfWithId(uid)
+    const shelf = await users.getShelfWithId(uid);
 
-    res.json(shelf)
+    res.json(shelf);
   },
-
-
 
   /**
 
@@ -138,27 +125,26 @@ module.exports = {
     }
     
   */
-  getNearbyUsers: async(req, res) => {
+  getNearbyUsers: async (req, res) => {
+    console.log("Got request");
 
-    console.log("Got request")
+    const beaconId = req.body.beaconId;
+    const major = req.body.beaconMajor;
+    const minor = req.body.beaconMinor;
 
-    const beaconId = req.body.beaconId
-    const major = req.body.beaconMajor
-    const minor = req.body.beaconMinor
+    if (!beaconId || !major || !minor) {
+      res.status(400).send();
+      return;
+    }
 
-    if( !beaconId ||
-        !major ||
-        !minor) {
-          res.status(400).send()
-          return
-        }
+    const nearbyUsers = await users.getNearbyUsersFromShelf(
+      beaconId,
+      major,
+      minor
+    );
 
-    const nearbyUsers = await users.getNearbyUsersFromShelf(beaconId, major, minor)
-
-    res.json(nearbyUsers)
-
+    res.json(nearbyUsers);
   },
- 
 
   /**
 
@@ -175,48 +161,46 @@ module.exports = {
     }
     
   */
-  addUserData: async(req, res) => {
+  addUserData: async (req, res) => {
+    console.log("adding user data");
 
     //User ID
-    const userId = req.body.userId
+    const userId = req.body.userId;
 
     //Id of the beacon attempting to connect to
-    const beaconId = req.body.beaconId
+    const beaconId = req.body.beaconId;
 
     //Major and minor
-    const beaconMajor = req.body.beaconMajor
-    const beaconMinor = req.body.beaconMinor
+    const beaconMajor = req.body.beaconMajor;
+    const beaconMinor = req.body.beaconMinor;
 
     //If request sent without fields, return 400
-    if( !userId ||
-        !beaconId ||
-        !beaconMajor ||
-        !beaconMinor)
-      { 
-        res.status(400).send() 
-      }
+    if (!userId || !beaconId || !beaconMajor || !beaconMinor) {
+      res.status(400).send();
+    }
 
     //Get user object
-    let user = await users.getUserWithId(userId)
+    let user = await users.getUserWithId(userId);
     //Make sure that user obj exists
-    if(!user) res.status(400).send()
+    if (!user) res.status(400).send();
 
     //Get Beacon object
-    let shelf = await users.getShelf(beaconId, beaconMajor, beaconMinor)
+    let shelf = await users.getShelf(beaconId, beaconMajor, beaconMinor);
     //Make sure that beacon/shelf obj exists
-    if(!shelf) res.status(400).send()
+    if (!shelf) res.status(400).send();
 
     //Create user object to update shelf with
     const userObj = {
       id: user.uid,
-      name: user.firstName
-    }
+      name: user.firstName,
+    };
 
     //Update shelf with a user object (add user object)
-    await users.addUserToShelf(beaconId, beaconMajor, beaconMinor, userObj)
+    await users
+      .addUserToShelf(beaconId, beaconMajor, beaconMinor, userObj)
       .then(() => {
-        res.status(200).send()
-      })
+        res.status(200).send();
+      });
   },
 
   /**
@@ -234,51 +218,79 @@ module.exports = {
     }
     
   */
-  removeUserData: async(req, res) => {
-
+  removeUserData: async (req, res) => {
     //User ID
-    const userId = req.body.userId
+    const userId = req.body.userId;
 
     //Id of the beacon attempting to connect to
-    const beaconId = req.body.beaconId
+    const beaconId = req.body.beaconId;
 
     //Major and minor
-    const beaconMajor = req.body.beaconMajor
-    const beaconMinor = req.body.beaconMinor
-  
+    const beaconMajor = req.body.beaconMajor;
+    const beaconMinor = req.body.beaconMinor;
+
     //If request sent without fields, return 400
-    if( !userId ||
-        !beaconId ||
-        !beaconMajor ||
-        !beaconMinor)
-      { 
-        res.status(400).send() 
-      }
+    if (!userId || !beaconId || !beaconMajor || !beaconMinor) {
+      res.status(400).send();
+    }
 
     //Get user object
-    let user = await users.getUserWithId(userId)
+    let user = await users.getUserWithId(userId);
     //Make sure that user obj exists
-    if(!user) res.status(400).send()
+    if (!user) res.status(400).send();
 
     //Get Beacon object
-    let shelf = await users.getShelf(beaconId, beaconMajor, beaconMinor)
+    let shelf = await users.getShelf(beaconId, beaconMajor, beaconMinor);
     //Make sure that beacon/shelf obj exists
-    if(!shelf) res.status(400).send()
+    if (!shelf) res.status(400).send();
 
     //Create user object to update shelf with
     const userObj = {
       id: user.uid,
-      name: user.firstName
-    }
+      name: user.firstName,
+    };
 
     //Update shelf with a user object (remove user object)
-    await users.removeUserFromShelf(beaconId, beaconMajor, beaconMinor, userObj)
+    await users
+      .removeUserFromShelf(beaconId, beaconMajor, beaconMinor, userObj)
       .then(() => {
-        res.status(200).send()
-      })
+        res.status(200).send();
+      });
 
-    res.status(200).send()
-
+    res.status(200).send();
   },
 
-}
+  findSize: async (req, res) => {
+    console.log(req.body);
+    var payload = JSON.stringify({
+      shelf: req.body.shelf,
+    });
+
+    var options = {
+      hostname: "192.168.1.10",
+      path: "/",
+      method: "GET",
+    };
+
+    var httpRequest = http.request(options, function (response) {
+      console.log("STATUS", response.statusCode);
+      response.setEncoding("utf8");
+
+      response.on("data", function (chunk) {
+        console.log("BODY:", chunk);
+      });
+
+      response.on("end", function () {
+        console.log("No more data in response");
+        res.status(200).send();
+      });
+    });
+
+    httpRequest.on("error", function (e) {
+      console.log("Error with the request:", e.message);
+    });
+
+    httpRequest.write(payload);
+    httpRequest.end();
+  },
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,79 +1,45 @@
-const express   = require('express');
-const router    = express.Router();
-const helpers   = require('../helpers/helpers');
-
+const express = require("express");
+const router = express.Router();
+const helpers = require("../helpers/helpers");
 
 /**
  * Sends a test message
  * @route   /routes/test
  */
-router
-  .route('/test')
-  .get(
-    helpers.sendTestMsg
-  );
-
+router.route("/test").get(helpers.sendTestMsg);
 
 /**
  * Gets a user object
  * @route   /routes/getUser
  */
-router
-  .route('/getUser')
-    .post(
-      helpers.getUser
-    )
-
+router.route("/getUser").post(helpers.getUser);
 
 /**
  * Gets a shelf object
  * @route   /routes/getUser
  */
-router
-  .route('/getShelf')
-    .post(
-      helpers.getShelf
-    )
+router.route("/getShelf").post(helpers.getShelf);
 
-
-router 
-  .route('/getShelfWithId')
-  .post(
-    helpers.getShelfWithId
-  )
+router.route("/getShelfWithId").post(helpers.getShelfWithId);
 
 /**
  * Gets the array of nearby users of a Firestore beacon object
  * @route   /routes/getNearbyUsers
  */
-router
-  .route('/getNearbyUsers')
-  .post(
-    helpers.getNearbyUsers
-  )
+router.route("/getNearbyUsers").post(helpers.getNearbyUsers);
 
 /**
  * Adds a user to a list of nearby users on a Firestore beacon object
  * @route   /routes/addUser
  */
-router 
-.route('/addUser')
-  .post(
-    helpers.addUserData
-  )
+router.route("/addUser").post(helpers.addUserData);
 
 /**
  * Removes a user from a list of nearby users on a Firestore beacon object
  * @route   /routes/removeUser
  */
-router
-.route('/removeUser')
-  .post(
-    helpers.removeUserData
-  )
+router.route("/removeUser").post(helpers.removeUserData);
 
-
-
-
+router.route("/findSize").post(helpers.findSize);
 
 module.exports = router;
